93d1a6f528b92a3222a55eb62475f7ef01b5c476
Sofia Gordillo <sofia.tamara31@gmail.com>
2022-05-10 23:53:20 -0500

    intente hacer lo del DOM

--- javascript/script.js
+++ javascript/script.js
@@ -1,54 +1,30 @@
-/*
-//Simulacro 1 - Números de la suerte para la semana
-const suerte = 8
-
-function calcularsuerte(numero1,numero2){
-    let resultado = numero1 + numero2
-    console.log (resultado * suerte)
-}
-
-let numero1 = parseFloat(prompt("Ingrese su edad"))
-let numero2 = parseFloat(prompt("Ingrese su año de nacimiento para conocer su número de la suerte"))
-
-calcularsuerte (numero1,numero2)*/
-
-/*
-let numerossuerte = [1,2,3,4,94]
-let multiplicadores = [3,6]
-let divisores = [5,8,1000]
-
-let numerofinalsuertudo = numerossuerte.concat(multiplicadores.concat(divisores))
-
-numerossuerte.splice(1,3)
-
-console.log(numerofinalsuertudo + 4) */
-
-
-//Conoce los precios de los jugadores de fútbol activos
-
-class Jugadores {
-    constructor(nombre,precio,edad,equipo){
+class Jugador {
+    constructor(id, nombre, precio, edad, equipo) {
+        this.id = id
         this.nombre = nombre
-        this.precio = precio
+        this.precio = precio 
         this.edad = edad
         this.equipo = equipo
     }
 }
-    const jugador1 = new Jugadores ("Harry Kane", "100,00 millones de euros", 28, "Tottenham Hotspur")
-    const jugador2 = new Jugadores ("Mohamed Salah", "100,00 millones de euros",  29, "Liverpool")
-    const jugador3 = new Jugadores ("Phil Foden",  "90,00 millones de euros",  21, "Manchester City")
-    const jugador4 = new Jugadores ("Bruno Fernandes", "90,00 millones de euros", 27, "Manchester United")
-    const jugador5 = new Jugadores ("Kevin De bruyne", "90,00 millones de euros", 30, "Manchester City")
+const jugador1 = new Jugador(1, "Harry Kane", "100,00 millones de euros", 28, "Tottenham Hotspur")
+const jugador2 = new Jugador(2, "Mohamed Salah", "100,00 millones de euros", 29, "Liverpool")
+const jugador3 = new Jugador(3, "Phil Foden", "90,00 millones de euros", 21, "Manchester City")
+const jugador4 = new Jugador(4, "Bruno Fernandes", "90,00 millones de euros", 27, "Manchester United")
+const jugador5 = new Jugador(5, "Kevin De bruyne", "90,00 millones de euros", 30, "Manchester City")
 
-let top5 = prompt ("Ingrese un número para conocer el jugador del top")
-if (top5 == 1){
-    console.log (jugador1)
-}else if (top5 == 2){
-    console.log(jugador2)
-}else if (top5 == 3){
-    console.log(jugador3)
-}else if (top5 == 4){
-    console.log(jugador4)
-}else if (top5 == 5){
-    console.log(jugador5)
-}
+let Jugadores = [jugador1, jugador2, jugador3, jugador4, jugador5]
+
+let divJugadores = document.getElementById('divJugadores')
+
+jugadores.forEach(jugador => {
+    divJugadores.innerHTML += 
+    <div id= "jugador$(jugador.id)">
+        <div>
+            <p>Nombre: ${jugador.nombre}</p>
+            <p>Precio: ${jugador.precio}</p>
+            <p>Edad: ${jugador.edad}</p>
+            <p>Equipo: ${jugador.equipo}</p>
+        </div>
+    </div>
+    })
